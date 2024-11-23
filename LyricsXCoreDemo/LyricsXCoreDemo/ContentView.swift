9e558d4bd962a10466f03bbf64d3d9962f2d47e8
tisfeng <tisfeng@gmail.com>
2024-11-23 17:41:29 +0800

    feat: add LyricsXCoreDemo

---
+++ LyricsXCoreDemo/LyricsXCoreDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LyricsXCoreDemo
+//
+//  Created by tisfeng on 2024/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LyricsXCoreDemo/LyricsXCoreDemo/LyricsXCoreDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LyricsXCoreDemoApp.swift
+//  LyricsXCoreDemo
+//
+//  Created by tisfeng on 2024/11/23.
+//
+
+import SwiftUI
+
+@main
+struct LyricsXCoreDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
