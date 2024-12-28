c48fb5377ffb55c4fd83660531cec7c1e120f0a6
tisfeng <tisfeng@gmail.com>
2024-12-29 00:37:23 +0800

    update LyricsKit, update demo

--- LyricsXCoreDemo/LyricsXCoreDemo/LyricsXCoreDemoApp.swift
+++ LyricsXCoreDemo/LyricsXCoreDemo/LyricsXCoreDemoApp.swift
@@ -5,12 +5,23 @@
 //  Created by tisfeng on 2024/11/23.
 //
 
-import SwiftUI
 import LyricsService
+import MusicPlayer
+import SwiftUI
 
 @main
 struct LyricsXCoreDemoApp: App {
     @Environment(\.dismissWindow) var dismissWindow
+
+    let track = MusicTrack(
+        id: "1",
+        title: "一生不变",
+        album: "Purple Dream",
+        artist: "李克勤",
+        duration: 262
+    )
+
+    @StateObject private var searchState = SearchState()
 
     var body: some Scene {
         WindowGroup {
@@ -19,11 +30,26 @@ struct LyricsXCoreDemoApp: App {
         .windowStyle(.hiddenTitleBar)
 
         Window(String.searchLyrics, id: .searchLyrics) {
-            LyricsSearchView(searchText: "一生不变 李克勤") { lyrics in
+            LyricsSearchView(searchState: searchState) { lyrics in
                 dismissWindow(id: .searchLyrics)
-
                 print(lyrics)
             }
+            .task {
+                await searchLyrics()
+            }
+        }
+    }
+
+    /// Start searching lyrics
+    func searchLyrics() async {
+        searchState.searchText = track.searchText
+        await searchState.performSearch()
+
+        let sortedLyricsList = searchState.lyricsList.sortedByScore(track: track)
+
+        searchState.lyricsList = sortedLyricsList.map { lyrics in
+            lyrics.metadata[Lyrics.Metadata.Key("quality")] = lyrics.calculateMatchingScore(track: track)
+            return lyrics
         }
     }
 }
